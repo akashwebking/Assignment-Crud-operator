21b6d3c51097203008dd14d3c6e245adc6e27684
akashwebking <139610933+akashwebking@users.noreply.github.com>
2023-10-17 23:54:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import EmpListing from './EmpListing';
 import EmpCreate from './EmpCreate';
-import EmpDetail from './EmpDetail';
 import EmpEdit from './EmpEdit';
 
 function App() {
